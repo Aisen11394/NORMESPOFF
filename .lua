c6a9678038872dd08a68665ea7bba3ab54974c62
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-08-05 12:32:50 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,4 +1,4 @@
 while wait(1) do
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Aisen11394/NORMESP2/main/.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Aisen11394/NORMESP3/main/.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aisen11394/NORMESP1/main/.lua"))()
 end
